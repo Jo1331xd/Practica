1854b9b08cbc8827dcf61284e89e2f5b48c284ea
RMSE233 <Sequeirarm@uamv.edu.ni>
2025-05-13 08:28:10 -0600

    registro semanal de gastos de estudiantes de la UAM

---
+++ Ejercicio2.py
@@ -0,0 +1,4 @@
+#Registro semanal de gastos de estudiantes de la UAM
+# Se requiere un programa que permita controlar el registro semanal de gastos de estudiantes de la UAM.
+# El programa debe permitir ingresar el nombre del estudiante, la cantidad de gastos que desea registrar y el precio por gasto.
+print("Registro semanal de gastos de estudiantes de la UAM") 
